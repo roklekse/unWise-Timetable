b55e5ac9c8be9ddf5d2ef0dd5370dbe2ffefa3e8
rok <rok@debian>
2016-03-21 20:13:53 +0100

    Bugfix, cookies

--- content.js
+++ content.js
@@ -1,9 +1,13 @@
-if (typeof(once) !== 'undefined'){
-  $("#program").val("1");
-  $("#year").append("<option value='2'>2</option>");
-  $("#branch").append("<option value='3'>Splošna</option>");
-  $("#group").append("<option value='84' selected>M2-20</option>");
 
-  $("input[value='Prikaži tedenski urnik']").click();
-  once=true;
+if (typeof Cookies.get("refreshed") === "undefined"){
+	$("#program").val("1");
+	$("#year").append("<option value='2'>2</option>");
+	$("#branch").append("<option value='3'>Splošna</option>");
+	$("#group").append("<option value='84' selected>M2-20</option>");
+
+	$("input[value='Prikaži tedenski urnik']").click();
+	Cookies.set("refreshed", "true");
+}
+else{
+	Cookies.remove("refreshed");
 }
